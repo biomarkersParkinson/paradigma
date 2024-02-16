e402ca8cf259d9f5f5a9eb4d1de426c905b2561b
Vedran Kasalica <v.kasalica@esciencecenter.nl>
2024-02-16 12:07:38 +0100

    Add the first test

--- tests/test_dbpd.py
+++ tests/test_dbpd.py
@@ -1,1 +1,13 @@
+import tsdf
+import numpy as np
+import pandas as pd
+from dbpd.constants import DataColumns
+
 from dbpd import dbpd
+
+def test_transform_time_array(shared_datadir):
+    metadata_dict = tsdf.load_metadata_from_path(shared_datadir / 'WatchData.IMU.Week0.raw_segment0161_meta.json')
+
+    # Retrieve the metadata object we want, using the name of the binary as key
+    metadata_samples = metadata_dict["WatchData.IMU.Week0.raw_segment0161_samples.bin"]
+    data = tsdf.load_ndarray_from_binary(metadata_samples)
