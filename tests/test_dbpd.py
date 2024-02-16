361e6def06073b57a970ac66b0584f8e3827841f
Vedran Kasalica <v.kasalica@esciencecenter.nl>
2024-02-16 12:25:45 +0100

    Verify the test data shape

--- tests/test_dbpd.py
+++ tests/test_dbpd.py
@@ -6,8 +6,9 @@ from dbpd.constants import DataColumns
 from dbpd import dbpd
 
 def test_transform_time_array(shared_datadir):
-    metadata_dict = tsdf.load_metadata_from_path(shared_datadir / 'WatchData.IMU.Week0.raw_segment0161_meta.json')
+    metadata_dict = tsdf.load_metadata_from_path(shared_datadir / 'PPG/WatchData.PPG.Week104.raw_segment0001_meta.json')
 
     # Retrieve the metadata object we want, using the name of the binary as key
-    metadata_samples = metadata_dict["WatchData.IMU.Week0.raw_segment0161_samples.bin"]
+    metadata_samples = metadata_dict["WatchData.PPG.Week104.raw_segment0001_time.bin"]
     data = tsdf.load_ndarray_from_binary(metadata_samples)
+    assert data.shape == (64775,)
