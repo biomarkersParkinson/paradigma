8d9ea50c2af0679b5c12a63aef6fe19f823c817f
Vedran Kasalica <v.kasalica@esciencecenter.nl>
2024-04-15 14:20:08 +0200

    Fix a copy/paste error

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -58,7 +58,8 @@ def test_2_extract_features_gait_output(shared_datadir):
 
     # Temporary path to store the output of the notebook
     input_path = os.path.join(shared_datadir, "2.preprocessed_data/gait")
-    
+    tmp_output_folder = create_tmp_folder_if_not_exists(shared_datadir, "tmp")
+    new_output_path = os.path.join(tmp_output_folder, step_dir)
 
     execute_notebook(shared_datadir, "2.extract_features_gait", input_path, new_output_path)
 
