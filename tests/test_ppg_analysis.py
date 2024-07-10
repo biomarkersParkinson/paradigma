586de430789c79dde53db7c6b24ef8ecd214367a
Vedran Kasalica <v.kasalica@esciencecenter.nl>
2024-07-10 09:17:20 +0200

    Introduce temp comparison cells to the PPG Notebook

--- tests/test_ppg_analysis.py
+++ tests/test_ppg_analysis.py
@@ -24,6 +24,11 @@ ppg_binaries_pairs: list[tuple[str, str]] = [
 ]
 imu_binaries_pairs: list[tuple[str, str]] = acceleration_binaries_pairs + ppg_binaries_pairs
 
+imu_new_pairs: list[tuple[str, str]] = [
+        # ("acceleration_meta.json", "acceleration_samples.bin"),
+        ("PPG_meta.json", "PPG_samples.bin"),
+]
+
 
 def test_1_imu_preprocessing(shared_datadir: Path):
     """
@@ -41,4 +46,4 @@ def test_1_imu_preprocessing(shared_datadir: Path):
     imu_config = IMUPreprocessingConfig()
     metadatas_ppg, metadatas_imu = scan_and_sync_segments(input_path_ppg, input_path_imu)
     preprocess_ppg_data(metadatas_ppg[0], metadatas_imu[0], tested_output_path, ppg_config, imu_config)
-    compare_data(reference_output_path, tested_output_path, imu_binaries_pairs)
+    compare_data(reference_output_path, tested_output_path, imu_new_pairs)
