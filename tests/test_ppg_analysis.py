09e05ad94efd713a8c5ab076e5cd15c895e589dd
Vedran Kasalica <v.kasalica@esciencecenter.nl>
2024-08-13 15:23:22 +0200

    Add a ppg feature extraction test

--- tests/test_ppg_analysis.py
+++ tests/test_ppg_analysis.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
+from dbpd.heart_rate_analysis_config import HeartRateFeatureExtractionConfig
 from dbpd.preprocessing_config import PPGPreprocessingConfig, IMUPreprocessingConfig
 from dbpd.ppg_preprocessing import *
 from dbpd.imu_preprocessing import *
 from dbpd.gait_analysis import *
 from dbpd.gait_analysis_config import *
+from dbpd.heart_rate_analysis import *
 from test_notebooks import compare_data
 
 
@@ -14,10 +16,6 @@ acceleration_binaries_pairs: list[tuple[str, str]] = [
         ("acceleration_meta.json", "acceleration_samples.bin"),
         ("acceleration_meta.json", "acceleration_time.bin"),
 ]
-acceleration_filt_binaries_pairs: list[tuple[str, str]] = [
-        ("acceleration_filt_meta.json", "acceleration_filt_samples.bin"),
-        ("acceleration_filt_meta.json", "acceleration_filt_time.bin"),
-]
 ppg_binaries_pairs: list[tuple[str, str]] = [
         ("PPG_meta.json", "PPG_samples.bin"),
         ("PPG_meta.json", "PPG_time.bin"),
@@ -25,7 +23,7 @@ ppg_binaries_pairs: list[tuple[str, str]] = [
 imu_binaries_pairs: list[tuple[str, str]] = acceleration_binaries_pairs + ppg_binaries_pairs
 
 
-def test_1_imu_preprocessing(shared_datadir: Path):
+def test_imu_preprocessing(shared_datadir: Path):
     """
     This function is used to evaluate the output of the preprocessing function. It evaluates it by comparing the output to a reference output.
     """
@@ -42,3 +40,20 @@ def test_1_imu_preprocessing(shared_datadir: Path):
     metadatas_ppg, metadatas_imu = scan_and_sync_segments(input_path_ppg, input_path_imu)
     preprocess_ppg_data(metadatas_ppg[0], metadatas_imu[0], tested_output_path, ppg_config, imu_config)
     compare_data(reference_output_path, tested_output_path, imu_binaries_pairs)
+
+
+def test_acc_feature_extraction(shared_datadir: Path):
+        """
+        This function is used to evaluate the output of the feature extraction function. It evaluates it by comparing the output to a reference output.
+        """
+        input_dir_name: str = "2.preprocessed_data"
+        output_dir_name: str = "3.extracted_features"
+        classifier_path = shared_datadir / "ppg_quality_classifier.pkl"
+        
+        input_path = shared_datadir / input_dir_name / "ppg"
+        reference_output_path = shared_datadir / output_dir_name / "ppg"
+        
+
+        config = HeartRateFeatureExtractionConfig()
+        extract_signal_quality_features(input_path, classifier_path, reference_output_path, config)
+        compare_data(reference_output_path, reference_output_path, imu_binaries_pairs)
