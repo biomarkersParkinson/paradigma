d1e0b4bacbf6653ad769d1fd4b0d740509754474
nienketimmermans <65012819+nienketimmermans@users.noreply.github.com>
2024-10-14 15:29:21 +0200

    run preprocessing and tests

---
+++ tests/test_tremor_analysis.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+from paradigma.imu_preprocessing import preprocess_imu_data_io
+from paradigma.preprocessing_config import IMUPreprocessingConfig
+from test_notebooks import compare_data
+
+
+# Mappings between the metadata and binary files
+accelerometer_binaries_pairs: list[tuple[str, str]] = [
+        ("accelerometer_meta.json", "accelerometer_samples.bin"),
+        ("accelerometer_meta.json", "accelerometer_time.bin"),
+    ]
+gyroscope_binaries_pairs: list[tuple[str, str]] = [
+        ("gyroscope_meta.json", "gyroscope_samples.bin"),
+        ("gyroscope_meta.json", "gyroscope_time.bin"),
+    ]
+imu_binaries_pairs: list[tuple[str, str]] = accelerometer_binaries_pairs + gyroscope_binaries_pairs
+
+
+def test_1_imu_preprocessing_outputs(shared_datadir: Path):
+    """
+    This function is used to evaluate the output of the preprocessing function. It evaluates it by comparing the output to a reference output.
+    """
+    input_dir_name: str = "1.sensor_data"
+    output_dir_name: str = "2.preprocessed_data"
+
+    input_path = shared_datadir / input_dir_name / "imu"
+    reference_output_path = shared_datadir / output_dir_name / "tremor"
+    tested_output_path = reference_output_path / "test-output"
+
+    config = IMUPreprocessingConfig()
+    preprocess_imu_data_io(input_path, tested_output_path, config)
+    compare_data(reference_output_path, tested_output_path, imu_binaries_pairs)
