fc38130a145ee4793ab88bec5459c954f5f46ef4
Erikpostt <erik_post_6@hotmail.com>
2025-01-09 13:37:39 +0100

    Change test data with new classifier

--- tests/test_gait_analysis.py
+++ tests/test_gait_analysis.py
@@ -34,6 +34,8 @@ gyroscope_binaries_pairs: list[tuple[str, str]] = [
     ]
 imu_binaries_pairs: list[tuple[str, str]] = accelerometer_binaries_pairs + gyroscope_binaries_pairs
 
+path_to_assets = Path('../src/paradigma/assets')
+
 
 def test_2_extract_features_gait_output(shared_datadir: Path):
     """
@@ -69,15 +71,14 @@ def test_3_gait_detection_output(shared_datadir: Path):
     input_dir_name: str = "3.extracted_features"
     output_dir_name: str = "4.predictions"
     data_type: str = "gait"
-    classifier_package_filename: str = "gait_detection_package.pkl"
+    classifier_package_filename: str = "gait_detection_clf_package.pkl"
 
     # Temporary path to store the output of the notebook
     path_to_feature_input = shared_datadir / input_dir_name / data_type
     path_to_reference_output = shared_datadir / output_dir_name / data_type
     path_to_tested_output = path_to_reference_output / "test-output"
-    path_to_classifier_package = '../src/paradigma/assets'
 
-    full_path_to_classifier_package = os.path.join(path_to_classifier_package, classifier_package_filename)
+    full_path_to_classifier_package = path_to_assets / classifier_package_filename
 
     config = GaitDetectionConfig()
     detect_gait_io(
@@ -101,7 +102,7 @@ def test_4_extract_features_arm_activity_output(shared_datadir: Path):
     input_dir_name: str = "2.preprocessed_data"
     output_dir_name: str = "3.extracted_features"
     data_type: str = "gait"
-    threshold_filename: str = "gait_detection_threshold.txt"
+    classifier_package_filename: str = "gait_filtering_clf_package.pkl"
 
     # Temporary path to store the output of the notebook
     path_to_timestamp_input = shared_datadir / input_dir_name / "imu"
@@ -109,14 +110,14 @@ def test_4_extract_features_arm_activity_output(shared_datadir: Path):
     path_to_reference_output = shared_datadir / output_dir_name / data_type
     path_to_tested_output = path_to_reference_output / "test-output"
 
-    full_path_to_threshold = shared_datadir / "0.classification" / data_type / "thresholds" / threshold_filename
+    full_path_to_classifier_package = path_to_assets / classifier_package_filename
 
     config = ArmActivityFeatureExtractionConfig()
     extract_arm_activity_features_io(
         config=config, 
         path_to_timestamp_input=path_to_timestamp_input, 
         path_to_prediction_input=path_to_prediction_input, 
-        full_path_to_threshold=full_path_to_threshold, 
+        full_path_to_classifier_package=full_path_to_classifier_package, 
         path_to_output=path_to_tested_output
     )
     compare_data(
@@ -141,9 +142,8 @@ def test_5_arm_swing_detection_output(shared_datadir: Path):
     path_to_prediction_input = shared_datadir / input_dir_name / data_type
     path_to_reference_output = shared_datadir / output_dir_name / data_type
     path_to_tested_output = path_to_reference_output / "test-output"
-    path_to_classifier_package = '../src/paradigma/assets'
 
-    full_path_to_classifier_package = full_path_to_classifier_package = os.path.join(path_to_classifier_package, classifier_package_filename)
+    full_path_to_classifier_package = path_to_assets / classifier_package_filename
 
     config = FilteringGaitConfig()
     filter_gait_io(
